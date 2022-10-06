a1609839a47ca94020aefe0b0ea5a280364007a6
Matt Radtke <more>
2022-10-06 10:13:31 +0100

    Shader class created

--- Lab1/main.cpp
+++ Lab1/main.cpp
@@ -9,94 +9,14 @@
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtc/type_ptr.hpp>
+#include "Shader.cpp"
 
 // Macro for indexing vertex buffer
 #define BUFFER_OFFSET(i) ((char *)NULL + (i))
 
 using namespace std;
 
-GLuint shaderProgramID1;
-
-
-// Shader Functions- click on + to expand
-#pragma region SHADER_FUNCTIONS
-static void AddShader(GLuint ShaderProgram, const char* pShaderText, GLenum ShaderType)
-{
-	// create a shader object
-    GLuint ShaderObj = glCreateShader(ShaderType);
-
-    if (ShaderObj == 0) {
-        fprintf(stderr, "Error creating shader type %d\n", ShaderType);
-        exit(0);
-    }
-	// Bind the source code to the shader, this happens before compilation
-	glShaderSource(ShaderObj, 1, (const GLchar**)&pShaderText, NULL);
-	// compile the shader and check for errors
-    glCompileShader(ShaderObj);
-    GLint success;
-	// check for shader related errors using glGetShaderiv
-    glGetShaderiv(ShaderObj, GL_COMPILE_STATUS, &success);
-    if (!success) {
-        GLchar InfoLog[1024];
-        glGetShaderInfoLog(ShaderObj, 1024, NULL, InfoLog);
-        fprintf(stderr, "Error compiling shader type %d: '%s'\n", ShaderType, InfoLog);
-        exit(1);
-    }
-	// Attach the compiled shader object to the program object
-    glAttachShader(ShaderProgram, ShaderObj);
-}
-
-GLuint CompileShaders(const char* vertexShaderPath, const char* fragmentShaderPath)
-{
-	std::ifstream vertexStream(vertexShaderPath);
-	std::string vertexShader((std::istreambuf_iterator<char>(vertexStream)),
-		std::istreambuf_iterator<char>());
-
-	std::ifstream fragmentStream(fragmentShaderPath);
-	std::string fragmentShader((std::istreambuf_iterator<char>(fragmentStream)),
-		std::istreambuf_iterator<char>());
-
-	//Start the process of setting up our shaders by creating a program ID
-	//Note: we will link all the shaders together into this ID
-    GLuint shaderProgramID = glCreateProgram();
-    if (shaderProgramID == 0) {
-        fprintf(stderr, "Error creating shader program\n");
-        exit(1);
-    }
-
-	// Create two shader objects, one for the vertex, and one for the fragment shader
-    AddShader(shaderProgramID, vertexShader.c_str(), GL_VERTEX_SHADER);
-    AddShader(shaderProgramID, fragmentShader.c_str(), GL_FRAGMENT_SHADER);
-
-    GLint Success = 0;
-    GLchar ErrorLog[1024] = { 0 };
-
-
-	// After compiling all shader objects and attaching them to the program, we can finally link it
-    glLinkProgram(shaderProgramID);
-	// check for program related errors using glGetProgramiv
-    glGetProgramiv(shaderProgramID, GL_LINK_STATUS, &Success);
-	if (Success == 0) {
-		glGetProgramInfoLog(shaderProgramID, sizeof(ErrorLog), NULL, ErrorLog);
-		fprintf(stderr, "Error linking shader program: '%s'\n", ErrorLog);
-        exit(1);
-	}
-
-	// program has been successfully linked but needs to be validated to check whether the program can execute given the current pipeline state
-    glValidateProgram(shaderProgramID);
-	// check for program related errors using glGetProgramiv
-    glGetProgramiv(shaderProgramID, GL_VALIDATE_STATUS, &Success);
-    if (!Success) {
-        glGetProgramInfoLog(shaderProgramID, sizeof(ErrorLog), NULL, ErrorLog);
-        fprintf(stderr, "Invalid shader program: '%s'\n", ErrorLog);
-        exit(1);
-    }
-	// Finally, use the linked shader program
-	// Note: this program will stay in effect for all draw calls until you replace it with another or explicitly disable its use
-    glUseProgram(shaderProgramID);
-	return shaderProgramID;
-}
-#pragma endregion SHADER_FUNCTIONS
+Shader* myShader;
 
 // VBO Functions - click on + to expand
 #pragma region VBO_FUNCTIONS
@@ -285,7 +205,7 @@ void display(){
 
 	glClear(GL_COLOR_BUFFER_BIT);
 	auto timeValue = glutGet(GLUT_ELAPSED_TIME);
-	int timeLocation = glGetUniformLocation(shaderProgramID1, "time");
+	int timeLocation = glGetUniformLocation(myShader->GetShaderProgramID(), "time");
 	glUniform1f(timeLocation, timeValue);
 	glutPostRedisplay();
 	// NB: Make the call to draw the geometry in the currently activated vertex buffer. This is where the GPU starts to work!
@@ -335,7 +255,8 @@ void init()
 	//int length = mySize<GLfloat*>(triangles);
 
 	// Set up the shaders
-	shaderProgramID1 = CompileShaders("./vertexshader.txt", "./fragmentshader.txt");
+	myShader = new Shader("./vertexshader.txt", "./fragmentshader.txt", true);
+	//shaderProgramID1 = CompileShaders("./vertexshader.txt", "./fragmentshader.txt");
 	//GLuint shaderProgramID2 = CompileShaders("./vertexshader2.txt", "./fragmentshader2.txt");
 	// Put the vertices and colors into a vertex buffer object
 	//generateObjectBuffer(vertices, colors);
@@ -346,13 +267,12 @@ void init()
 	//glUniform1f(time, timeValue);
 
 
-	AddTriangles(allVertices, shaderProgramID1, 2, 6);
+	AddTriangles(allVertices, myShader->GetShaderProgramID(), 2, 6);
 	int numTriangles = 2;
 	int vertexCount = numTriangles * 3;
 
 	auto timeValue = glutGet(GLUT_ELAPSED_TIME);
-	int timeLocation = glGetUniformLocation(shaderProgramID1, "time");
-	glUniform1f(timeLocation, timeValue);
+	myShader->SetUniform1f("time", timeValue);
 	
 
 	glm::mat4x4 MVP;
@@ -381,13 +301,17 @@ void init()
 	MVP[1][3] = 0.5;
 	MVP[2][3] = 0.5;
 
-	GLuint MatrixID = glGetUniformLocation(shaderProgramID1, "MVP");
-	GLuint MatrixIdentity = glGetUniformLocation(shaderProgramID1, "Identity");
-	// Send our transformation to the currently bound shader,
-	// in the "MVP" uniform
-	// For each model you render, since the MVP will be different (at least the M part)
-	glUniformMatrix4fv(MatrixID, 1, GL_FALSE, &MVP[0][0]);
-	glUniformMatrix4fv(MatrixIdentity, 1, GL_FALSE, &identity[0][0]);
+
+	//GLuint MatrixID = glGetUniformLocation(myShader->GetShaderProgramID(), "MVP");
+	//GLuint MatrixIdentity = glGetUniformLocation(myShader->GetShaderProgramID(), "Identity");
+	//// Send our transformation to the currently bound shader,
+	//// in the "MVP" uniform
+	//// For each model you render, since the MVP will be different (at least the M part)
+	//glUniformMatrix4fv(MatrixID, 1, GL_FALSE, &MVP[0][0]);
+	//glUniformMatrix4fv(MatrixIdentity, 1, GL_FALSE, &identity[0][0]);
+
+	myShader->SetUniformMatrix4fv("MVP", &MVP);
+	myShader->SetUniformMatrix4fv("Identity", &identity);
 
 	//LearnOpenGLTest(vertices, shaderProgramID1, 1, 3);
 	//LearnOpenGLTest(allVertices, shaderProgramID1, numTriangles, vertexCount);
